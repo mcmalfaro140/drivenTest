2e72cb4c769907fd1de9c7a586dc67001f11f4c9
mcmalfaro140 <mcmalfaro@hotmail.com>
2020-08-12 15:18:51 -0700

    radio buttons added

--- components/Board.js
+++ components/Board.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Input, InputGroup } from 'reactstrap';
+import { Container, Row, Col, Input, InputGroup, ButtonGroup, Button } from 'reactstrap';
 
 export default class Board extends React.Component{
     constructor(props){
@@ -14,18 +14,45 @@ export default class Board extends React.Component{
     col_even = (e) => {
         let dot = false
         let myclass = "red_piece"
+
+        let changeColor = (i) => {
+            switch (i) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+        }
+
         if(e < 2){
             dot = true
         }else if(this.state.N-3 < e){
             myclass = "black_piece"
             dot = true
         }
+        
         let my_col = [];
         for (let index = 0; index < this.state.N; index++) {
             if(index % 2 === 0){
-                my_col.push(<Col className="tiles black"><div className={dot ? myclass : "" }></div></Col>)
+                my_col.push(<Col className="tiles black">{dot ? (<div className={dot ? myclass : "" }>
+                    <Row>
+                        <Col><Input type="radio" name="color1" onClick={() => changeColor(0)}/></Col>
+                        <Col><Input type="radio" name="color2" onClick={() => changeColor(1)}/></Col>
+                        <Col><Input type="radio" name="color3" onClick={() => changeColor(2)}/></Col>
+                    </Row>
+                </div>) : (<></>)}</Col>)
             }else{
-                my_col.push(<Col className="tiles"><div className={dot ? myclass : "" }></div></Col>)
+                my_col.push(<Col className="tiles">{dot ? (<div className={dot ? myclass : "" }>
+                    <Row>
+                        <Col><Input type="radio" name="radio1"/></Col>
+                        <Col><Input type="radio" name="radio2"/></Col>
+                        <Col><Input type="radio" name="radio3"/></Col>
+                    </Row>
+                </div>) : (<></>)}</Col>)
             }
         }
         return my_col;
@@ -43,9 +70,21 @@ export default class Board extends React.Component{
         let my_col = [];
         for (let index = 0; index < this.state.N; index++) {
             if(index % 2 === 1){
-                my_col.push(<Col className="tiles black"><div className={dot ? myclass : "" }></div></Col>)
+                my_col.push(<Col className="tiles black">{dot ? (<div className={dot ? myclass : "" }>
+                        <Row>
+                            <Col><Input type="radio" name="radio1"/></Col>
+                            <Col><Input type="radio" name="radio1"/></Col>
+                            <Col><Input type="radio" name="radio1"/></Col>
+                        </Row>
+                </div>) : (<></>)}</Col>)
             }else{
-                my_col.push(<Col className="tiles"><div className={dot ? myclass : "" }></div></Col>)
+                my_col.push(<Col className="tiles">{dot ? (<div className={dot ? myclass : "" }>
+                    <Row>
+                        <Col><Input type="radio" name="radio1"/></Col>
+                        <Col><Input type="radio" name="radio1"/></Col>
+                        <Col><Input type="radio" name="radio1"/></Col>
+                    </Row>
+                </div>) : (<></>)}</Col>)
             }
            
         }
